a0e776cf6fe6a0525667135833b79366ef92282f
horsicq <horsicq@gmail.com>
2019-07-07 14:54:20 +0200

    Fix

--- pe/QtPlugin.h
+++ pe/QtPlugin.h
@@ -25,8 +25,8 @@
 //#include "PluginMainWindow.h"
 
 #define plugin_name "PE Viewer"
-#define plugin_version 1 // 0.01
-#define plugin_version_str "0.01" // 0.01
+#define plugin_version 2 // 0.02
+#define plugin_version_str "0.02" // 0.02
 
 namespace QtPlugin
 {
